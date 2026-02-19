c7bd0b14d291da4156241c7fe54542d1ebdbfcdc
JayashreeMurugan13 <jshreemurugan@gmail.com>
2026-02-19 23:46:18 +0530

    urls.py

--- onlineCourseApp-main/onlineCourseApp-main/FinalProject/urls.py
+++ onlineCourseApp-main/onlineCourseApp-main/FinalProject/urls.py
@@ -5,20 +5,24 @@ from django.contrib import admin
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    
     path('admin/', admin.site.urls),
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    path('', views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
+
+    # Course detail
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
+
+    # Enroll
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+
+    # Submit exam
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 
-    # <HINT> Create a route for show_exam_result view
+    # Show exam result (FIXED)
+    path('<int:course_id>/result/', views.show_exam_result, name='show_exam_result'),
 
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
